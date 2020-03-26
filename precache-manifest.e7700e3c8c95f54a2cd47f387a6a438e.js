251b1bd3bfc437bbd47f6de1370c5c45ae9a62b8
LightblueLotus <shahrevajehgeneral@gmail.com>
2020-03-26 08:47:20 +0430

    initial commit

---
+++ precache-manifest.e7700e3c8c95f54a2cd47f387a6a438e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fbeb9ccb96e1168a0294bf11731e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5148be219d8045ba955",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc6da9059afb5d33a255",
+    "url": "/static/js/2.08efcb0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08efcb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5148be219d8045ba955",
+    "url": "/static/js/main.48c52d95.chunk.js"
+  },
+  {
+    "revision": "2825b092296fb8257827",
+    "url": "/static/js/runtime-main.c089c2a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
